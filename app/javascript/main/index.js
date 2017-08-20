a7e5c002f41ac5d360092551e9f6f9b87ba496da
irenliber <irr.morgo@gmail.com>
2017-08-20 19:11:58 +0700

    base structure was created

---
+++ app/javascript/main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import '@blueprintjs/core/dist/blueprint.css';
+
+ReactDOM.render(
+  <div className="container">
+    <div> hello! </div>
+  </div>,
+  document.body.appendChild(document.createElement('div')),
+
+);
